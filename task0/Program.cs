66b91402dfb0db0256c97e45c11a1044674dd813
Dima Mynka <mdv8804@gmail.com>
2023-01-28 17:31:55 +0200

    sqr

---
+++ task0/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу которая на вход принимает число и выдает его квадрат(число умноженное нансамо себя)
+
+Console.Clear(); // oshistit console
+Console.Write("Put number"); // napisat v console tekst
+int number = int.Parse(Console.ReadLine()); // Что бы взять с концоля числа
+int sqr = number*number;
+Console.WriteLine("number "  + number + " qvals = " + sqr);
