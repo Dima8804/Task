53ecd931fb0522366d3ac1ace4ee0b0afc20e287
Dima Mynka <mdv8804@gmail.com>
2023-01-28 19:15:56 +0200

    days of week

---
+++ task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу которая будет выдавать названия дня недели по заданному номеру
+
+Console.Clear();
+Console.Write("put number: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number == 1)
+{
+    Console.WriteLine(" Monday");
+}
+if(number == 2)
+{
+    Console.WriteLine(" Tuesday ");
+}
+if(number == 3)
+{
+    Console.WriteLine("Wensday ");
+}
+if(number == 4)
+{
+    Console.WriteLine("Thursday ");
+}
+if(number == 5)
+{
+    Console.WriteLine("Friday ");
+}
+if(number == 6)
+{
+    Console.WriteLine("Saturday ");
+}
+if(number == 7)
+{
+    Console.WriteLine("Sanday ");
+}
+if(number > 7)
+{
+    Console.WriteLine(" not  ");
+}
